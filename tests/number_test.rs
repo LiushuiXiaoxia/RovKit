43c287cbda2b3bb942dbee8e9caba62f4531c472
xiaqiulei <xiaqiulei@bilibili.com>
2025-07-03 00:17:33 +0800

    test

--- tests/number_test.rs
+++ tests/number_test.rs
@@ -15,7 +15,7 @@ mod tests {
 
     #[test]
     fn test_rounding() {
-        assert_eq!(round(3.14159, 2), 3.14);
+        assert_eq!(round(std::f64::consts::PI, 2), 3.14);
         assert_eq!(ceil(2.3), 3.0);
         assert_eq!(floor(2.9), 2.0);
         assert_eq!(abs(-5.0), 5.0);
@@ -23,7 +23,7 @@ mod tests {
 
     #[test]
     fn test_formatting() {
-        assert_eq!(format_float(3.14159, 3), "3.142");
+        assert_eq!(format_float(std::f64::consts::PI, 3), "3.142");
         assert_eq!(format_percent(0.1234, 1), "12.3%");
         assert_eq!(format_thousands(1234567.89), "1,234,567.89");
     }
